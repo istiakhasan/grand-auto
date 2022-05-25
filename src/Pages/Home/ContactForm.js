ebb47d974f64544a767616eb9bb421ae1313df15
Istiak <istieak.hasan1996@gmail.com>
2022-05-26 02:52:04 +0600

    send contact form data to server

--- src/Pages/Home/ContactForm.js
+++ src/Pages/Home/ContactForm.js
@@ -1,16 +1,41 @@
 import React from "react";
+import { toast } from "react-toastify";
 
 const ContactForm = () => {
+  const handleSendMessage=(e)=>{
+           e.preventDefault()
+           const name=e.target.name.value
+           const email=e.target.email.value
+           const subject=e.target.subject.value
+           const message=e.target.message.value
+
+           const data={
+               name,email,subject,message
+           }
+           fetch('http://localhost:4000/sendmessage',{
+             method:"POST",
+             headers:{
+               'content-type':'application/json'
+             }
+           })
+           .then(res=>res.json())
+           .then(data=>{
+             console.log(data)
+             toast.success("Message send successfully")
+             e.target.reset()
+           })
+  }
   return (
     <div className=" mx-10 lg:mx-0">
       <h2 className="font-bold text-2xl text-primary mb-5 mt-10">GET IN TOUCH</h2>
-      <form action="">
+      <form onSubmit={handleSendMessage}>
           <div className="lg:grid grid-cols-2 gap-10">
 
         <div className="form-control w-full mb-10 lg:mb-0">
           <input
             type="text"
             placeholder="Name"
+            required
             name="name"
             className="input input-bordered w-full text-lg "
             />
@@ -19,6 +44,7 @@ const ContactForm = () => {
           <input
             type="email"
             placeholder="Email"
+
             required
             name="email"
             className="input input-bordered w-full text-lg"
@@ -35,10 +61,10 @@ const ContactForm = () => {
             />
         </div>
             <div className="form-control w-full ">
-            <textarea name="message" rows="6" className="textarea textarea-bordered text-lg" placeholder="Write Message"></textarea>
+            <textarea name="message" rows="6" required className="textarea textarea-bordered text-lg" placeholder="Write Message"></textarea>
         </div>
-      </form>
       <button className="btn btn-primary mt-5 px-12 mb-10">SEND A MESSAGE</button>
+      </form>
     </div>
   );
 };
